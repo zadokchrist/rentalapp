4c12f8303639ce994f3d48bf3ff08c17e913cd4b
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-01-30 09:13:18 +0300

    enabling payments

--- initialize.php
+++ initialize.php
@@ -1,6 +1,7 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_daniel','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
 if(!defined('base_url')) define('base_url','http://3.216.182.63/rentalapp/');
+// if(!defined('base_url')) define('base_url','http://localhost/rentalapp/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
